fb6bb6093391d38c813e3bbcc78f46579b401ca8
iyed <iyed.mohamed@isimg.tn>
2024-11-28 14:01:53 +0100

    Main Prosit 10 , Set

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,28 +19,38 @@ public class Main {
         Department d2 = new Department(2, "Finance", 50);
         Department d3 = new Department(5, "Agriculture", 30);
         Department d4 = new Department(3, "IT", 20);
+
         departments.ajouterDepartement(d1);
         departments.ajouterDepartement(d2);
         departments.ajouterDepartement(d3);
         departments.ajouterDepartement(d4);
 
-        departments.rechercherDepartement(d3);
+        System.out.println("**** departments are: ****");
+        departments.displayDepartement();
+        System.out.println("*******");
+        System.out.println(departments.rechercherDepartement(d3));
+        System.out.println("*******");
+        System.out.println(departments.rechercherDepartement("Finance"));
 
-        departments.rechercherDepartement("Finance");
-
+        System.out.println("*******");
+        TreeSet<Department> sortedDep = departments.trierDepartementById();
+        for (Department d : sortedDep) {
+            System.out.println(d);
+        }
+        System.out.println("*******");
+        departments.supprimerDepartement(d2);
+        System.out.println("departments are after removing: ");
         departments.displayDepartement();
 
-        departments.trierDepartementById();
 
-
-
-        // Display a specific employee
-        System.out.println("Display specific employee:");
-        company.displayEmployer(e1);  // Should print details of employee e1
-
-        // Attempt to display an employee not in the list
-        Employee e4 = new Employee(4, "Med", "Nihel",27, "Journalism", 4);
-        System.out.println("Attempt to display an employee not in the list:");
-        company.displayEmployer(e4);  // Should print "Employee not found."
+      // Display a specific employee
+//        System.out.println("Display employee:");
+//        company.displayEmployer(e1);
+//
+//        // Attempt to display an employee not in the list
+//        Employee e4 = new Employee(4, "Med", "Nihel",27, "Journalism", 4);
+//        System.out.println("Attempt to display an employee not in the list:");
+//        company.displayEmployer(e4);  // Should print "Employee not found."
+//        company.trierEmployerParId();
     }
 }
